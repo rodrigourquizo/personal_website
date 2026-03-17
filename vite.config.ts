f669cc3f423354bceb809e569b2475f058fdfafc
rodrigourquizo <rodrigourquizo96@gmail.com>
2026-03-17 13:22:15 -0500

    new version in docs/

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/personal_website/',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     host: "::",
     port: 8080,
